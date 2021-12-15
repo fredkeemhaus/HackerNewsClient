9befbf843bae5303aa910cf8963099f2b97ac186
fredkeemhaus <fredkeemhaus@gmail.com>
2021-12-15 23:35:53 +0900

    router

--- app.js
+++ app.js
@@ -11,32 +11,51 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-window.addEventListener("hashchange", function () {
+  newsList.push("<ul>");
+
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
+      <li>
+          <a href=#${newsFeed[i].id}>
+              ${newsFeed[i].title} (${newsFeed[i].comments_count})
+          </a>
+      </li>
+    `);
+  }
+
+  newsList.push("</ul>");
+
+  container.innerHTML = newsList.join("");
+}
+
+function newsDetail() {
   const id = location.hash.substr(1);
 
   const newsContent = getData(CONTENT_URL.replace("@id", id));
-  const title = document.createElement("h1");
 
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-});
+  container.innerHTML = `
+  <h1>${newsContent.title}</h1>
 
-for (let i = 0; i < 10; i++) {
-  const div = document.createElement("div");
-
-  div.innerHTML = `
-    <li>
-        <a href=#${newsFeed[i].id}>
-            ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>
-    </li>
+  <div>
+    <a href="#"> 목록으로</a>
+  </div>
   `;
-
-  ul.appendChild(div.firstElementChild);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener("hashchange", router);
+
+router();
